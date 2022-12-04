be1770d490681accdfff039a5c633dac9fa5321a
oltap <oltap@LAPTOP-RAUC0V1F>
2022-12-04 15:56:04 +0100

    slider news

--- main.js
+++ main.js
@@ -16,3 +16,21 @@ responsive:{
 }
 })
 })
+$(function(){
+    $('.owl-carousel2').owlCarousel({
+loop:true,
+margin:10,
+nav:true,
+responsive:{
+0:{
+    items:1
+},
+600:{
+    items:3
+},
+1000:{
+    items:3
+}
+}
+})
+})
