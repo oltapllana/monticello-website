f424290dad290ee31b46733e7581a99166a245ca
oltap <oltap@LAPTOP-RAUC0V1F>
2022-12-04 23:04:18 +0100

    responsive

--- script/main.js
+++ script/main.js
@@ -3,17 +3,22 @@ $(function(){
 loop:true,
 margin:10,
 nav:true,
-responsive:{
-0:{
-    items:1
-},
-600:{
-    items:3
-},
-1000:{
-    items:1
-}
-}
+responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:1,
+            nav:false
+        },
+        1000:{
+            items:1,
+            nav:true,
+            loop:false
+        }
+    }
 })
 })
 $(function(){
